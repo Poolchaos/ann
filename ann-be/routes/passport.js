3c0bb40f24cad81b537795f763fff7dc5b470d94
Phillip-Juan <phillip-juan@zailab.com>
2021-02-13 12:33:15 +0200

    add routing

--- ann-be/routes/passport.js
+++ ann-be/routes/passport.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const AnonymousModel = require('../models/anonymous-model');
 const RegistrationModel = require('../models/registration-model');
 const ObjectID = require('mongodb').ObjectID;
-var jwt = require('jsonwebtoken');
 
 //Set up default mongoose connection
 const mongoDB = 'mongodb://localhost:27017/ann-projector';
@@ -17,45 +16,49 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 router.post(
-  '/user-registration/submit',
+  '/submit',
   function (req, res, next) {
     // Gather the jwt access token from the request header
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1];
     
-    if (token == null) return res.sendStatus(401) // if there isn't any token
+    if (token == null) return res.sendStatus(401);
 
     AnonymousModel.find({}, function (err, docs) {
-      console.log(' ::>> docs >>>> ', docs[0], docs);
-  
+      if (err) console.log('err = ', err);
       if (docs[0]) {
-        if (token === docs[0].anonymous) {
-          next();
+        if (token.indexOf(docs[0].anonymous) >= 0) {
+          return next();
         }
-      } else {
-        console.log(' ::>> anonymous tokens don`t match ');
-        return res.sendStatus(401)
       }
+      console.log(' ::>> anonymous tokens don`t match ');
+      return res.sendStatus(401)
     });
   },
-  function(req, res, next){
+  function(req, res, next) {
     try {
       let user = req.body;
       user._id = new ObjectID();
-      
-      var user_instance = new RegistrationModel(user);
-      // Save the new model instance, passing a callback
-      user_instance.save(function (err) {
-        if (err) {
-          console.log(err);
+
+      RegistrationModel.find({ email: user.email }, function (err, docs) {
+        if (docs.length === 0) {
+          // todo: send email on confirmation
+          
+          var user_instance = new RegistrationModel(user);
+          user_instance.save(function (err) {
+            if (err) {
+              console.log(err);
+              return res.sendStatus(405);
+            }
+            return res.send(user);
+          });
+        } else {
           return res.sendStatus(405);
         }
-        return res.send(user);
-        // saved!
       });
 
     } catch(e) {
-
+      console.log(' ::>> failer to register ');
     }
   });
 
--- ann-fe/src/app.ts
+++ ann-fe/src/app.ts
@@ -1,12 +1,29 @@
 import { HttpClient } from 'aurelia-http-client';
 import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
 import { UserRegistrationSettings } from 'registration/user-registration-settings';
 
 @autoinject()
 export class App {
   public message = 'Hello World!';
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private router: Router
+  ) {}
+
+  public configureRouter(config, router): void {
+    config.title = 'ANN';
+    config.map([
+      { route: ['', 'home'],   name: 'home',         moduleId: PLATFORM.moduleName('home/home')                                                   },
+      { route: 'login',        name: 'login',        moduleId: PLATFORM.moduleName('login/login'),               nav: true, title: 'Login'        },
+      { route: 'registration', name: 'registration', moduleId: PLATFORM.moduleName('registration/registration'), nav: true, title: 'Registration' },
+      { route: 'dashboard',    name: 'dashboard',    moduleId: PLATFORM.moduleName('dashboard/dashboard'),       nav: true, title: 'Dashboard'    },
+    ]);
+    this.router = router;
+  }
 
   public activate(): void {
     this.retrieveUsers();
@@ -51,7 +68,7 @@ export class App {
       contactNumbers: ["0712569431"]
     };
 
-    this.httpClient.createRequest('http://localhost:3000/passport/user-registration/submit')
+    this.httpClient.createRequest('http://localhost:3000/passport/submit')
       .asPost()
       .withContent(user)
       .withHeader('Content-Type', 'application/json')
@@ -65,5 +82,21 @@ export class App {
           console.warn(' ::>> error ', error);
         }
       );
+  }
+
+  public goToHome(): void {
+    this.router.navigate('home');
+  }
+
+  public goToLogin(): void {
+    this.router.navigate('login');
+  }
+
+  public goToRegistration(): void {
+    this.router.navigate('registration');
+  }
+
+  public goToDashboard(): void {
+    this.router.navigate('dashboard');
   }
 }
