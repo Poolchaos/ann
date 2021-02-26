a1c77081e7e93bbf3af52d2295079e5edfed0264
Phillip-Juan <phillip-juan@zailab.com>
2021-02-26 17:54:39 +0200

    rename app

---
+++ ZANet-fe/src/dashboard/dashboard-routes.ts
@@ -0,0 +1,135 @@
+import { RouteConfig } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
+export class DashboardRoutes {
+
+  public static get routes(): RouteConfig[] {
+    return this.adminRoutes().concat(this.journalistRoutes(), this.voiceOverRoutes(), this.defaultUserRoutes());
+  }
+
+  private static adminRoutes(): RouteConfig[] {
+    return [{
+      route: 'admin',
+      name: 'admin',
+      moduleId: PLATFORM.moduleName('features/admin/admin'),
+      nav: true,
+      title: 'Dashboard',
+      auth: true,
+      settings: {
+        access: ['Admin']
+      }
+    }, {
+      route: 'users',
+      name: 'users',
+      moduleId: PLATFORM.moduleName('features/admin/users/users'),
+      nav: true,
+      title: 'Users',
+      auth: true,
+      settings: {
+        access: ['Admin']
+      }
+    }];
+  }
+
+  private static journalistRoutes(): RouteConfig[] {
+    return [{
+      route: 'journalist',
+      name: 'journalist',
+      moduleId: PLATFORM.moduleName('features/journalist/journalist'),
+      nav: true,
+      title: 'Dashboard',
+      auth: true,
+      settings: {
+        access: ['Journalist']
+      }
+    }, {
+      route: 'articles',
+      name: 'articles',
+      moduleId: PLATFORM.moduleName('features/journalist/articles/articles'),
+      nav: true,
+      title: 'Articles',
+      auth: true,
+      settings: {
+        access: ['Admin', 'Journalist', 'VoiceOver', 'DEFAULT_USER']
+      }
+    }, {
+      route: 'create-article',
+      name: 'create-article',
+      moduleId: PLATFORM.moduleName('features/journalist/articles/create-article/create-article'),
+      nav: true,
+      title: 'Create Article',
+      auth: true,
+      settings: {
+        access: ['Journalist']
+      }
+    }, {
+      route: 'edit-article',
+      name: 'edit-article',
+      moduleId: PLATFORM.moduleName('features/journalist/articles/create-article/create-article'),
+      nav: true,
+      title: 'Edit Article',
+      auth: true,
+      settings: {
+        access: ['Journalist']
+      }
+    }];
+  }
+
+  private static voiceOverRoutes(): RouteConfig[] {
+    return [{
+      route: 'voice-over',
+      name: 'voice-over',
+      moduleId: PLATFORM.moduleName('features/voice-over/voice-over'),
+      nav: true,
+      title: 'Dashboard',
+      auth: true,
+      settings: {
+        access: ['Voice-Over']
+      }
+    }];
+  }
+
+  private static defaultUserRoutes(): RouteConfig[] {
+    return [{
+      route: 'default_user',
+      name: 'default_user',
+      moduleId: PLATFORM.moduleName('features/default_user/default_user'),
+      nav: true,
+      title: 'Dashboard',
+      auth: true,
+      settings: {
+        access: ['DEFAULT_USER']
+      }
+    }, {
+      route: 'cart',
+      name: 'cart',
+      moduleId: PLATFORM.moduleName('features/default_user/cart/cart'),
+      nav: true,
+      title: 'Cart',
+      auth: true,
+      settings: {
+        access: ['DEFAULT_USER']
+      }
+    }, {
+      route: 'purchases',
+      name: 'purchases',
+      moduleId: PLATFORM.moduleName('features/default_user/purchases/purchases'),
+      nav: true,
+      title: 'Purchases',
+      auth: true,
+      settings: {
+        access: ['Admin', 'DEFAULT_USER']
+      }
+    }, {
+      route: 'checkout-complete',
+      name: 'checkout-complete',
+      moduleId: PLATFORM.moduleName('features/default_user/cart/checkout-complete/checkout-complete'),
+      nav: true,
+      title: 'Checkout complete',
+      auth: true,
+      settings: {
+        access: ['DEFAULT_USER']
+      }
+    }];
+  }
+}
---
+++ ZANet-fe/src/registration/complete-registration/complete-registration.ts
@@ -0,0 +1,48 @@
+import { autoinject } from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+import { CookieService } from 'services/cookie-service';
+import { RegistrationService } from 'registration/registration-service';
+
+@autoinject()
+export class CompleteRegistration {
+
+  public password: string;
+  public confirmPassword: string;
+  public complete: boolean;
+
+  private token: string;
+
+  constructor(
+    private cookieService: CookieService,
+    private router: Router,
+    private registrationService: RegistrationService
+  ) {}
+
+  public activate(params: {[key: string]: string}): void {
+    if (params.token) {
+      this.token = params.token;
+    } else {
+      this.complete = true;
+      setTimeout(() => {
+        this.router.navigate('home');
+      }, 3000);
+    }
+  }
+
+  public completeRegistration(): void {
+    this.registrationService
+      .completeRegistration(
+        this.token,
+        this.password
+      )
+      .then(() => {
+        console.log(' ::>> registration Complete >>> navigating to login');
+        this.cookieService.eraseCookie('ZANet-registration');
+        this.router.navigate('login');
+      })
+      .catch(() => {
+        this.router.navigate('login');
+      });
+  }
+}
